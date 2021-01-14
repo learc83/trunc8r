79c3294100527666dc14cede14d5a55a91cdbe40
seth <learc83@gmail.com>
2021-01-13 20:13:35 -0500

    Change links controller to return error messages
    Return error messages if url is blank or if url is invalid

--- trunc8r/app/controllers/links_controller.rb
+++ trunc8r/app/controllers/links_controller.rb
@@ -1,6 +1,11 @@
 class LinksController < ApplicationController
   def create
-    link = Link.create(url: 'http://test.com')
-    render json: { slug: link.slug }, status: :created
+    link = Link.new(url: params[:link][:url])
+
+    if link.save
+      render json: { slug: link.slug }, status: :created
+    else
+      render json: { errors: link.errors.full_messages }, status: 400
+    end
   end
 end
--- trunc8r/spec/requests/links_request_spec.rb
+++ trunc8r/spec/requests/links_request_spec.rb
@@ -5,13 +5,38 @@ RSpec.describe 'Links', type: :request do
     it 'creates a new link and returns a slug' do
       headers = { 'ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json' }
 
+      url = "https://google.com/test#{rand(999_999_999)}"
+
       expect do
-        post links_path params: { link: { url: 'http://google.com' } }, headers: headers
+        post links_path params: { link: { url: url } }, headers: headers
       end.to change { Link.count }.by(1)
 
+      expect(Link.last.url).to eql(url)
       expect(response).to have_http_status(:created)
       expect(response.content_type).to include('application/json')
       expect(JSON.parse(response.body)['slug']).to eql(Link.last.slug)
+    end
+
+    it 'returns an error if the url is blank' do
+      headers = { 'ACCEPT' => 'application/json', 'CONTENT_TYPE' => 'application/json' }
+
+      expect do
+        post links_path params: { link: { url: '' } }, headers: headers
+      end.to change { Link.count }.by(0)
+
+      expect(response).to have_http_status(400)
+      expect(response.content_type).to include('application/json')
+      expect(JSON.parse(response.body)['errors']).to include("Url can't be blank")
+    end
+
+    it 'returns an error if the url is invalid' do
+      expect do
+        post links_path params: { link: { url: 'htt://google.com' } }, headers: headers
+      end.to change { Link.count }.by(0)
+
+      expect(response).to have_http_status(400)
+      expect(response.content_type).to include('application/json')
+      expect(JSON.parse(response.body)['errors']).to include('Url must be a valid url (must start with http(s)://)')
     end
   end
 end
