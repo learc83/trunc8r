2ae717ce953dfe34b3299d4f995361a6651dde41
seth <learc83@gmail.com>
2021-01-15 21:40:19 -0500

    Add static content to index.html.erb
    Add static copy, css, and a sample text field to the index

--- trunc8r/app/javascript/components/LinkShortener.jsx
+++ trunc8r/app/javascript/components/LinkShortener.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default function LinkShortener() {
-  return <div className="">hello there</div>;
+  return <div className=""></div>;
 }
