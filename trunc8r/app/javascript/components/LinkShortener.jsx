d6c7d5680a7a1995e13b4767e8ccbbf4ff082034
seth <learc83@gmail.com>
2021-01-16 00:44:32 -0500

    Change css to make index responsive

--- trunc8r/app/javascript/components/LinkShortener.jsx
+++ trunc8r/app/javascript/components/LinkShortener.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default function LinkShortener() {
-  return <div className=""></div>;
+  return (
+    <section class="link_shortener">
+      <input
+        type="text"
+        placeholder="http://google.com"
+        class="link_shortener__input"
+      />
+      <button type="button" class="link_shortener__button">
+        Shorten!
+      </button>
+    </section>
+  );
 }
