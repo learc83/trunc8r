7db94fd69a451d2f66df55f5ef9952d176f8ffaa
seth <learc83@gmail.com>
2021-01-18 09:30:00 -0500

    Enable final integration test

--- trunc8r/spec/features/index_spec.rb
+++ trunc8r/spec/features/index_spec.rb
@@ -53,17 +53,17 @@ feature 'Index' do
     expect(first('.link_shortener__response')).to have_content("Here's your short url:")
   end
 
-  # scenario 'Visiting a short url redirects you to the long url', js: true do
-  #   long_url = 'http://example.com'
-  #   visit root_path
+  scenario 'Visiting a short url redirects you to the long url', js: true do
+    long_url = 'http://example.com'
+    visit root_path
 
-  #   first('.link_shortener__input').set(long_url)
-  #   first('.link_shortener__button').click
+    first('.link_shortener__input').set(long_url)
+    first('.link_shortener__button').click
 
-  #   short_url = find('#short_url')['innerHTML']
+    short_url = find('#short_url')['innerHTML']
 
-  #   visit short_url
+    visit short_url
 
-  #   expect(page.current_url).to eq(long_url + '/')
-  # end
+    expect(page.current_url).to eq(long_url + '/')
+  end
 end
