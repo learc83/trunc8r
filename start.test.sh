32b34b9b951fa5b96b47130381a3adfc32d354b1
seth <learc83@gmail.com>
2021-01-18 10:26:34 -0500

    Fix typo in rspec script

--- start.test.sh
+++ start.test.sh
@@ -7,16 +7,7 @@ bundle install
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rake db:drop db:create db:migrate
 rails assets:precompile
 printf "%s\n\n\n\n\n\n\n\n\n\n\n\n\n"
-echo "Runing rspec tests.................................................."
-# echo "Runing model tests.................................................."
+echo "Running rspec tests.................................................."
 rspec -c -fd
-# rspec spec/models -fd
-
-# echo "Runing request tests.................................................."
-# rspec spec/requests -fd
-
-# echo "Runing feature tests using selenium.................................................."
-# rspec spec/features -fd
-
 echo "Rspec tests complete.................................................."
 
